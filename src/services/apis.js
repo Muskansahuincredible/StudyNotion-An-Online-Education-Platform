073b4f9a5daf895f75e9ee06bc514e3cc265ed7a
Dushyant Bhutyani <dushyantb2003@gmail.com>
2024-05-19 20:21:33 +0530

    Fixed bug: Signup Bug

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,5 @@
-const BASE_URL = "https://localhost:4000/api/v1"
+// const BASE_URL = "http://localhost:4000/api/v1"
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 // AUTH ENDPOINTS
 export const endpoints = {
