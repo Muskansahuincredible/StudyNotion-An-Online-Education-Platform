53417943abaf4f036b835941561af1bbd5a42512
Anukriti <anukriti.student.che21@itbhu.ac.in>
2024-05-30 23:12:19 +0530

    Adding hover effect on cards

--- src/components/core/HomePage/CourseCard.jsx
+++ src/components/core/HomePage/CourseCard.jsx
@@ -11,24 +11,24 @@ const CourseCard = ({cardData, currentCard, setCurrentCard}) => {
         currentCard === cardData?.heading
           ? "bg-white shadow-[12px_12px_0_0] shadow-yellow-50"
           : "bg-richblack-800"
-      }  text-richblack-25 h-[300px] box-border cursor-pointer`}
+      }  text-richblack-25 h-[300px] box-border cursor-pointer hover:shadow-xl hover:border-brown-50 hover:scale-[1.07] transition-all ease-in-out`}
       onClick={() => setCurrentCard(cardData?.heading)}
     >
       <div className="border-b-[2px] border-richblack-400 border-dashed h-[80%] p-6 flex flex-col gap-3">
         <div
           className={` ${
             currentCard === cardData?.heading && "text-richblack-800"
-          } font-semibold text-[20px]`}
+          } font-semibold text-[20px] hover:font-extrabold`}
         >
           {cardData?.heading}
         </div>
 
-        <div className="text-richblack-400">{cardData?.description}</div>
+        <div className={`text-richblack-400  transition-all ease-linear ${ currentCard === cardData?.heading ? "hover:text-black" : "hover:text-white"}`}>{cardData?.description}</div>
       </div>
 
       <div
-        className={`flex justify-between ${
-          currentCard === cardData?.heading ? "text-blue-300" : "text-richblack-300"
+        className={`flex justify-between  transition-all ease-linear ${
+          currentCard === cardData?.heading ? "text-blue-300 hover:text-blue-500" : "text-richblack-300 hover:text-white"
         } px-6 py-3 font-medium`}
       >
         {/* Level */}
