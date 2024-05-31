8ef6df3cf00e016e9e9fadf95e90c64839ae8bac
mukilan2808 <mukilan2808@gmail.com>
2024-05-31 05:20:07 +0000

    completed

--- src/components/common/ReviewSlider.jsx
+++ src/components/common/ReviewSlider.jsx
@@ -91,7 +91,7 @@ function ReviewSlider() {
           {reviews.map((review, i) => {
             return (
               <SwiperSlide key={i}>
-                <div className="flex flex-col gap-3 bg-richblack-800 p-3 text-[14px] text-richblack-25 h-[250px]">
+                <div className="flex flex-col gap-3 bg-richblack-800 p-3 text-[14px] text-richblack-25 h-[200px]">
                   <div className="flex items-center gap-4">
                     <img
                       src={
