aea38485934e8e791430faecc00aa97eff151787
Poushmita Paul <poushmitap@gmail.com>
2024-06-01 12:37:24 +0530

    update responsiveness

--- src/index.js
+++ src/index.js
@@ -16,12 +16,12 @@ const store = configureStore({
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-  <Provider store = {store}>
-    <BrowserRouter>
+   <Provider store = {store}>
+      <BrowserRouter>
         <App />
         <Toaster/>
       </BrowserRouter>
-  </Provider>
+   </Provider>
     
     
   </React.StrictMode>
