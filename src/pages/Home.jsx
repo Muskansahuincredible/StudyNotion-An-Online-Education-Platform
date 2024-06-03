8d2175d5f0d898da50544c6829a89028ab635a07
Aryan <aryansharma220318@gmail.com>
2024-06-03 21:50:54 +0530

    added navigatin button for blogs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -177,7 +177,7 @@ function Home() {
       </div>
 
       {/* Section 3 */}
-      <div className="relative mx-auto my-20 flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 bg-richblack-900 text-white">
+      <div className="relative mx-auto my-20 flex max-w-maxContent flex-col items-center justify-between gap-8 bg-richblack-900 text-white">
         {/* Become a instructor section */}
         <InstructorSection />
 
