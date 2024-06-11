1f3f607a2a8244a79896f452e38aabf2a81bc639
Sailaja Adapa <adapasailaja17@gmail.com>
2024-06-11 21:05:33 +0530

    Added Toastify Message

--- src/pages/Rateus.jsx
+++ src/pages/Rateus.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 import "./rateus.css";
 
 function RateUsComponent({ previousContent }) {
@@ -15,19 +17,23 @@ function RateUsComponent({ previousContent }) {
   };
 
   const handleSubmit = () => {
-    setRating(0);
-    setFeedback("");
-    setShowModal(false); 
+    if (rating > 0 || feedback) {
+      toast.success("Thanks for your Feedback :)");
+      setRating(0);
+      setFeedback("");
+      setShowModal(false); 
+    } else {
+      toast.error("Please enter all details :(");
+    }
   };
 
   return (
     <>
       <div className="rate-us-page">
-  <div className="rate-us-container">
-    <h2 className="rate-us-heading">Rate Our Website</h2>
-    <div className="star-rating">
-      {/* Stars go here */}
-      {[1, 2, 3, 4, 5].map((star) => (
+        <div className="rate-us-container">
+          <h2 className="rate-us-heading">Rate Our Website</h2>
+          <div className="star-rating">
+            {[1, 2, 3, 4, 5].map((star) => (
               <span
                 key={star}
                 className={star <= rating ? "star filled" : "star"}
@@ -36,21 +42,21 @@ function RateUsComponent({ previousContent }) {
                 ★
               </span>
             ))}
-    </div>
-    <textarea
-      className="feedback-textarea"
-      placeholder="Write your feedback here..."
-      value={feedback}
-      onChange={handleFeedbackChange}
-    ></textarea>
-    <div className="submit-container">
-      <button className="submit-button" onClick={handleSubmit}>
-        Submit
-      </button>
-    </div>
-  </div>
-</div>
-
+          </div>
+          <textarea
+            className="feedback-textarea"
+            placeholder="Write your feedback here..."
+            value={feedback}
+            onChange={handleFeedbackChange}
+          ></textarea>
+          <div className="submit-container">
+            <button className="submit-button" onClick={handleSubmit}>
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+      <ToastContainer position="top-center" />
     </>
   );
 }
